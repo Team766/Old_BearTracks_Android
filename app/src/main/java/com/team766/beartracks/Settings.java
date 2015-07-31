4b86cb11ed7aa42824a2c41392c57aca31d44130
tommypacker <tommypacker@gmail.com>
2015-07-31 12:33:00 -0700

    Begin work on Settings tab

---
+++ app/src/main/java/com/team766/beartracks/Settings.java
@@ -0,0 +1,7 @@
+package com.team766.beartracks;
+
+/**
+ * Created by tommypacker on 7/31/15.
+ */
+public class Settings {
+}
---
+++ app/src/main/java/com/team766/beartracks/SettingsFragment.java
@@ -0,0 +1,9 @@
+package com.team766.beartracks;
+
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by tommypacker on 7/31/15.
+ */
+public class SettingsFragment extends PreferenceFragment{
+}
